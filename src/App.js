f87694ae21769fea9fb3aa040c4acb1b9edc5f36
Vinicius Rocha <mvinicius.gbo@gmail.com>
2018-06-26 19:07:22 -0300

    Enable Sidebar Toggle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import "./App.css";
 import SearchField from "./search-field/Search-field";
 import SideBar from "./side-bar/Side-bar";
@@ -9,34 +10,36 @@ class DestinationCard extends Component {
   }
 }
 
-class App extends Component {
-  render() {
-    let name = "Vinicius";
-
-    return (
-      <div className="App">
-        <SideBar />
-        <main className="Content-container otl-green suspendend-view ">
-          <div className="generic-container white-text">
-            <div className="top-bar">
-              <button type="button">
-                <i className="material-icons">menu</i>
-              </button>
-            </div>
+const App = ({ appState, toggleSidebar }) => {
+  return (
+    <div id="app" className="App">
+      <SideBar active={appState.sidebarActive} />
+      <main className="Content-container otl-green suspendend-view ">
+        <div className="generic-container white-text">
+          <div className="top-bar">
+            <button onClick={toggleSidebar} type="button">
+              <i className="material-icons">menu</i>
+            </button>
           </div>
-          <SearchField />
-          <section className="">
-            {/* <div className="cards-grid">
+        </div>
+        <SearchField />
+        <section className="">
+          {/* <div className="cards-grid">
               <DestinationCard />
               <DestinationCard />
               <DestinationCard />
             </div> */}
-          </section>
-        </main>
-        {/* <footer className="App-footer">Vinicius</footer> */}
-      </div>
-    );
-  }
-}
+        </section>
+      </main>
+      {/* <footer className="App-footer">Vinicius</footer> */}
+    </div>
+  );
+};
+
+App.render = node => {
+  return props => {
+    ReactDOM.render(<App {...props} />, node);
+  };
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+import App from "./App";
+const render = App.render(document.getElementById("root"));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let appState = {
+  sidebarActive: false
+};
+
+const toggleSidebar = () => {
+  appState.sidebarActive = !appState.sidebarActive;
+  render({ appState, toggleSidebar });
+};
+
+render({ appState, toggleSidebar });
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
