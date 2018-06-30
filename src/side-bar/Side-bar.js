e26cd4c5857d727db40911f183727f796baaf9ff
Vinicius Rocha <mvinicius.gbo@gmail.com>
2018-06-29 23:22:59 -0300

    Add typefaces from Google Fonts, Set Nav links style

--- src/side-bar/Side-bar.js
+++ src/side-bar/Side-bar.js
@@ -3,9 +3,9 @@ import "./Side-bar.css";
 
 const SideBar = ({ active }) => (
   <div className={`Side-bar ${active ? "active" : ""}`}>
-    <header className="App-header">LOGO {active}</header>
+    {/* <header className="App-header">LOGO {active}</header> */}
     <nav className="App-nav">
-      <ul className="fw600">
+      <ul className="fw600 dim-text">
         <li>
           <a href="javascript:void(0)">Explore</a>
         </li>
